5ebccb5774d6c1db15849ec0d3d33aa559f0bfda
Aravind Vasudevan <aravindv96@yahoo.com>
2017-10-22 13:27:01 +0530

    zoho question 8 solution added

--- zoho_solutions/q8.cc
+++ zoho_solutions/q8.cc
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <sstream>
+#include <cmath>
 
-std::string bottom_left_diamond(const int& n) {
+std::string bottom_left_diamond(const int& nstars) {
     std::stringstream output;
+    const int n = ceil(sqrt(nstars));
+    const int length = 2 * n - 1;
+    int print_start_indicator = n * n;
 
+    for (int i = 1; i <= length / 2 + 1; i++) {
+        output << std::string(n - i, ' ');
+        for (int j = 0; j < i; j++) {
+            output << (print_start_indicator-- > nstars ? "  " : "* ");
+        }
+        output << "\n";
+    }
 
+    for (int i = length / 2; i > 0; i--) {
+        output << std::string(n - i, ' ');
+        for (int j = 0; j < i; j++) {
+            output << (print_start_indicator-- > nstars ? "  " : "* ");
+        }
+        output << "\n";
+    }
 
     return output.str();
 }
