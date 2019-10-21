03eb9d8ef9e471d5d1145d7cf7bf73a4a26ec25d
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2019-10-20 22:32:54 -0400

    Switched to boolean array based solution

--- problems/leetcode/JumpGame.java
+++ problems/leetcode/JumpGame.java
@@ -5,17 +5,25 @@ class Solution {
       }
 
       int N = nums.length;
-      int[] path = new int[N];
+      boolean[] path = new boolean[N];
 
+      path[0] = true;
       for (int i = 0; i < N; i++) {
-        if (i != 0 && path[i] == 0) {
+        if (!path[i]) {
           return false;
         }
 
         int endRange = i + 1 + nums[i];
-        for (int j = i + 1; j < N && j < endRange; path[j++]++);
+        for (int j = i + 1; j < N && j < endRange; j++) {
+          path[j] = true;
+        }
+
+        if (path[N - 1]) {
+          return true;
+        }
       }
 
       return true;
     }
+}
 
