1190a5990b8b94856da5b00da9a1cfd9c58eaada
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2019-10-20 22:27:53 -0400

    Added Leetcode solutions

--- problems/leetcode/JumpGame.java
+++ problems/leetcode/JumpGame.java
@@ -8,16 +8,14 @@ class Solution {
       int[] path = new int[N];
 
       for (int i = 0; i < N; i++) {
+        if (i != 0 && path[i] == 0) {
+          return false;
+        }
+
         int endRange = i + 1 + nums[i];
         for (int j = i + 1; j < N && j < endRange; path[j++]++);
       }
 
-      for (int i = 1; i < N; i++) {
-        if (path[i] == 0) {
-          return false;
-        }
-      }
-
       return true;
     }
-}
+
