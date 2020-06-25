3b1b753b522c16d3aefab5a489a9d2b4568e3f23
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2020-06-25 19:37:22 -0400

    Create unique-paths.py

---
+++ problems/leetcode/unique-paths.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/unique-paths/
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        dp[0][1] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+
+        return dp[m][n]
