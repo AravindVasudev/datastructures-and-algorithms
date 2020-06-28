1f9dd525fbd2700a94369555b9dbf1c69dba9932
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2020-06-28 08:53:54 -0400

    Create letter-combinations-of-a-phone-number.py

---
+++ problems/leetcode/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        numToChar = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        
+        return self.generateCombinations(digits, 0, set(), numToChar)
+    
+    def generateCombinations(self, digits, cur, generated, numToChar):
+        if cur >= len(digits):
+            return []
+        
+        nextList = self.generateCombinations(digits, cur + 1, generated, numToChar)
+        
+        curList = []
+        for char in numToChar[digits[cur]]:
+            for item in nextList:
+                curList.append(char + item)
+                
+        return curList if curList else numToChar[digits[cur]]
