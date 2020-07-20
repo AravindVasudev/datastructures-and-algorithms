159688b336fb46495d0697770b43e801c624e69d
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2020-07-20 02:37:49 -0400

    Update asteroid-collision.py

--- problems/leetcode/asteroid-collision.py
+++ problems/leetcode/asteroid-collision.py
@@ -46,4 +46,21 @@ class Solution:
             prevCycle = curCycle
                 
         return prevCycle
-                    
+    
+#### EFFICIENT SOLUTION ####
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            while stack and asteroid < 0 < stack[-1]:
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -asteroid:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+                
+        return stack
+
