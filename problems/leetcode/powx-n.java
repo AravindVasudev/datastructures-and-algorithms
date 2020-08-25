b46987095bde3da35b647ed792e9cbf46064696a
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2020-08-25 01:29:51 -0400

    Create powx-n.java

---
+++ problems/leetcode/powx-n.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/powx-n/
+class Solution {
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        
+        return myPowHelper(x, n);
+    }
+    
+    private double myPowHelper(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        
+        if (n == 1) {
+            return x;
+        }
+        
+        double half = myPowHelper(x, n / 2);
+        double xPowerN = 1;
+        if (n % 2 != 0) {
+            xPowerN = x;
+        }
+        
+        xPowerN *= half * half;
+        return xPowerN;
+    }
+};
