730af0054bda96a9a284ca921b9eaa3c98ec5621
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2020-09-25 01:54:21 -0400

    Update longest-increasing-subsequence.java

--- problems/leetcode/longest-increasing-subsequence.java
+++ problems/leetcode/longest-increasing-subsequence.java
@@ -28,3 +28,31 @@ class Solution {
                                                  lengthOfLIS(nums, index + 1, prevIndex, memo));
     }
 }
+
+// Bottom-up
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        int maxAns = 1;
+
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int maxVal = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxVal = Math.max(maxVal, dp[j]);
+                }
+            }
+            
+            dp[i] = maxVal + 1;
+            maxAns = Math.max(maxAns, dp[i]);
+        }
+        
+        return maxAns;
+    }
+}
