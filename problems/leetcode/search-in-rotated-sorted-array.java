0bf39a70b10700fa0d0bb9a6f08c973c8e3b8e64
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-02-18 10:27:49 -0500

    Update search-in-rotated-sorted-array.java

--- problems/leetcode/search-in-rotated-sorted-array.java
+++ problems/leetcode/search-in-rotated-sorted-array.java
@@ -41,3 +41,34 @@ class Solution {
         return -1;
     }
 }
+
+// One pass
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (target == nums[mid]) {
+                return mid;
+            }
+            
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
