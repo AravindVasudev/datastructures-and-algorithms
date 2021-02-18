2eb32f374ba8ea95cb28b10940c0e7fdb95d03b9
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-02-18 14:30:55 -0500

    Create peak-index-in-a-mountain-array.py

---
+++ problems/leetcode/peak-index-in-a-mountain-array.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left, right = 0, len(arr) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+            
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid;
+                
+        return left
