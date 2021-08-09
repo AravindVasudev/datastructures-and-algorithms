ee6f838a39f52094c85ea8c763d5079df0ef87a0
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-08-09 16:53:19 -0700

    Update median-of-two-sorted-arrays.py

--- problems/leetcode/median-of-two-sorted-arrays.py
+++ problems/leetcode/median-of-two-sorted-arrays.py
@@ -1,25 +1,29 @@
 # https://leetcode.com/problems/median-of-two-sorted-arrays/
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums = []
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
         
-        nums1Index, nums2Index = 0, 0
-        nums1Length, nums2Length = len(nums1), len(nums2)
-
-        while nums1Index < nums1Length and nums2Index < nums2Length:
-            if nums1[nums1Index] < nums2[nums2Index]:
-                nums.append(nums1[nums1Index])
-                nums1Index += 1
-            else:
-                nums.append(nums2[nums2Index])
-                nums2Index += 1
+        low, high, mid = 0, len(nums1), (len(nums1) + len(nums2) + 1) // 2
+        while low <= high:
+            part1 = (low + high) // 2
+            part2 = mid - part1
+            
+            left1 = float('-inf') if part1 == 0 else nums1[part1 - 1]
+            right1 = float('inf') if part1 == len(nums1) else nums1[part1]
+            
+            left2 = float('-inf') if part2 == 0 else nums2[part2 - 1]
+            right2 = float('inf') if part2 == len(nums2) else nums2[part2]
+            
+            if left1 <= right2 and left2 <= right1:
+                if (len(nums1) + len(nums2)) % 2 == 0:
+                    return (max(left1, left2) + min(right1, right2)) / 2
+                else:
+                    return max(left1, left2)
                 
-        while nums1Index < nums1Length:
-            nums.append(nums1[nums1Index])
-            nums1Index += 1
-            
-        while nums2Index < nums2Length:
-            nums.append(nums2[nums2Index])
-            nums2Index += 1
-            
-        return nums[len(nums) // 2] * 0.5 + nums[(len(nums) - 1) // 2] * 0.5
+            if left1 > right2:
+                high = part1 - 1
+            else:
+                low = part1 + 1
+                
+        return -1
