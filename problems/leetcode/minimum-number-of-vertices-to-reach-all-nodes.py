6681e7386fe501ebc587317c0bf5022eb61fb9db
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-08-11 00:49:32 -0700

    Create minimum-number-of-vertices-to-reach-all-nodes.py

---
+++ problems/leetcode/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(n)]
+        for edge in edges:
+            graph[edge[0]].append(edge[1])
+            
+        visited = [False] * n
+        result = [False] * n
+        for node in range(n):
+            if visited[node]:
+                continue
+                
+            self.dfs(graph, node, visited, result)
+            result[node] = True
+
+        return [i for i, val in enumerate(result) if val]
+    
+    def dfs(self, graph: List[List[int]], node: int, visited: List[bool], result: List[bool]) -> None:
+        if result[node]:
+            result[node] = False
+
+        if visited[node]:
+            return
+        
+        visited[node] = True
+        for connection in graph[node]:
+            self.dfs(graph, connection, visited, result)
+
