ad0e2811d63671d381d86144e77ad76ac07b3695
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-08-23 16:55:50 -0700

    Update word-ladder.py

--- problems/leetcode/word-ladder.py
+++ problems/leetcode/word-ladder.py
@@ -30,3 +30,46 @@ class Solution:
         return 0
 
 # Optimized
+# Bidriectional BFS
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+        
+        if endWord not in wordSet:
+            return 0
+        
+        headVisited = {beginWord: 1}
+        tailVisited = {endWord: 1}
+        
+        headQ = deque()
+        headQ.append((beginWord, 1))
+        
+        tailQ = deque()
+        tailQ.append((endWord, 1))
+        
+        while headQ and tailQ:
+            found = self.bfs(headQ, headVisited, tailVisited, wordSet)
+            if found > 0:
+                return found
+            
+            found = self.bfs(tailQ, tailVisited, headVisited, wordSet)
+            if found > 0:
+                return found
+            
+        return 0
+    
+    def bfs(self, queue: deque[(str, int)], visited: dict[str, int], otherVisited: dict[str, int], wordSet: set[str]) -> int:
+        word, level = queue.popleft()
+        
+        for i in range(len(word)):
+            for char in 'abcdefghijklmnopqrstuvwxyz':
+                newWord = word[:i] + char + word[i+1:]
+                if newWord in wordSet:
+                    if newWord in otherVisited:
+                        return level + otherVisited[newWord]
+                    
+                    if newWord not in visited:
+                        visited[newWord] = level + 1
+                        queue.append((newWord, level + 1))
+                        
+        return 0
