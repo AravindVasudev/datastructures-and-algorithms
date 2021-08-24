a37dfcec15b3f6be9f34ff31b0cf64344798e121
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-08-23 18:52:09 -0700

    Update word-break-ii.py

--- problems/leetcode/word-break-ii.py
+++ problems/leetcode/word-break-ii.py
@@ -14,3 +14,26 @@ class Solution:
                 self.dfs(s[i:], wordSet, f"{path} {s[:i]}", result)
                 
         return result
+
+# Optimized
+# With DP memo
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        return self.dfs(s, wordSet, {})
+    
+    def dfs(self, s: str, wordSet: set[str], memo: Dict[str, List[str]]) -> List[str]:            
+        if s in memo:
+            return memo[s]
+        
+        if s == "":
+            return [""]
+        
+        result = []
+        for i in range(1, len(s) + 1):
+            word = s[:i]
+            if word in wordSet:
+                result.extend([word + (" " if suffix else "") + suffix for suffix in self.dfs(s[i:], wordSet, memo)])
+                
+        memo[s] = result
+        return result
