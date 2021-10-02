d502904e1d91dd018c1dd7eaa2c1ea1be71984eb
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-10-02 00:59:35 -0700

    Update permutations.py

--- problems/leetcode/permutations.py
+++ problems/leetcode/permutations.py
@@ -13,3 +13,20 @@ class Solution:
             nums[i] = num
             
         return permutations if permutation else [[]]
+
+# Alternative swap solution
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first = 0):
+            if first == len(nums):
+                result.append(list(nums))
+            
+            for i in range(first, len(nums)):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(first + 1)
+                nums[first], nums[i] = nums[i], nums[first]
+                
+        result = []
+        backtrack()
+        return result
+                
