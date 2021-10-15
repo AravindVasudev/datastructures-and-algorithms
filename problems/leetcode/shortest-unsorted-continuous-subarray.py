a88300dc0f0289336bf57aae162a9c68df5c1863
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2021-10-14 18:06:09 -0700

    Update shortest-unsorted-continuous-subarray.py

--- problems/leetcode/shortest-unsorted-continuous-subarray.py
+++ problems/leetcode/shortest-unsorted-continuous-subarray.py
@@ -12,3 +12,35 @@ class Solution:
         return max(0, end - start + 1)
         
 # Efficient Solution
+# Solution: https://leetcode.com/problems/shortest-unsorted-continuous-subarray/discuss/103067/Python-O(N)-with-O(1)-space-complexity.-No-sorting
+class Solution:
+    def findUnsortedSubarray(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        
+        left, right = 0, len(nums) - 1
+        
+        # Find the first non-ascending index
+        while left < len(nums) - 1 and nums[left] < nums[left + 1]:
+            left += 1
+            
+        # Find the first non-descending index from the last
+        while right > 0 and nums[right] >= nums[right - 1]:
+            right -= 1
+            
+        if left > right:
+            return 0
+        
+        temp = nums[left:right+1]
+        tempMin = min(temp)
+        tempMax = max(temp)
+        
+        # Move left until the minimum finds its position
+        while left > 0 and tempMin < nums[left - 1]:
+            left -= 1
+            
+        # Move right until the maximum finds its position
+        while right < len(nums) - 1 and tempMax > nums[right + 1]:
+            right += 1
+            
+        return right - left + 1
