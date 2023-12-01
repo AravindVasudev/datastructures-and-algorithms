c0eb70a4076c60b11b86fbed775ee7e3b55fc22c
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2023-12-01 22:42:21 +0530

    Update MergeTwoSortedList.java

--- problems/leetcode/MergeTwoSortedList.java
+++ problems/leetcode/MergeTwoSortedList.java
@@ -54,3 +54,21 @@ class Solution {
         return merged;
     }
 }
+
+// Python
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = cur = ListNode()
+
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+
+            cur = cur.next
+
+        cur.next = list1 or list2
+        return head.next
