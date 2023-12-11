150c2dd76b440e1949f02128c98dc69786482a9c
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2023-12-11 08:37:22 +0530

    Update longest-common-subsequence.py

--- problems/leetcode/longest-common-subsequence.py
+++ problems/leetcode/longest-common-subsequence.py
@@ -12,3 +12,39 @@ class Solution:
             return max(dfs(t1 + 1, t2), dfs(t1, t2 + 1))
         
         return dfs()
+
+# Bottom-Up
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        M, N = len(text1), len(text2)
+        memo = [[0] * (N + 1) for _ in range(M + 1)]
+
+        for i in range(1, M + 1):
+            for j in range(1, N + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    memo[i][j] = 1 + memo[i - 1][j - 1]
+                else:
+                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
+
+        return memo[M][N]
+
+# Bottom-Up Space Optimized
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if len(text1) < len(text2):
+            text1, text2 = text2, text1
+
+        M, N = len(text1), len(text2)
+        prevRow = [0] * (N + 1)
+        curRow = [0] * (N + 1)
+    
+        for i in range(1, M + 1):
+            for j in range(1, N + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    curRow[j] = 1 + prevRow[j - 1]
+                else:
+                    curRow[j] = max(prevRow[j], curRow[j - 1])
+
+            prevRow, curRow = curRow, prevRow
+
+        return prevRow[N]
