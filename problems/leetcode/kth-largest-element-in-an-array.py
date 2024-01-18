2bd844f80fcbc8c4f23664b2faf12502acc313b8
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-01-18 00:42:18 -0800

    Update kth-largest-element-in-an-array.py

--- problems/leetcode/kth-largest-element-in-an-array.py
+++ problems/leetcode/kth-largest-element-in-an-array.py
@@ -10,3 +10,25 @@ class Solution:
                 heapq.heappop(pqueue)
                 
         return heapq.heappop(pqueue)
+
+# Quick-Select
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return -1
+
+        pivot = random.choice(nums) # Pick a random pivot.
+
+        # Partition in descending order.
+        left = [x for x in nums if x > pivot]
+        mid = [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+
+        # Pick which half to look for.
+        L, M = len(left), len(mid)
+        if k <= L:
+            return self.findKthLargest(left, k)
+        elif k > L + M:
+            return self.findKthLargest(right, k - (L + M))
+        else:
+            return mid[0]
