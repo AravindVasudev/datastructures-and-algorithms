e8b3406a2efd714cec146062e77b0d70d34df016
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-01-23 19:57:36 -0800

    Update top-k-frequent-elements.py

--- problems/leetcode/top-k-frequent-elements.py
+++ problems/leetcode/top-k-frequent-elements.py
@@ -11,3 +11,25 @@ class Solution:
                 heapq.heappop(pqueue)
                 
         return [element for _, element in pqueue]
+
+# O(N) bucket sort solution
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # return [e for e, _ in Counter(nums).most_common(k)]
+        counter = Counter(nums)
+
+        # Bucket sort the results.
+        buckets = [[] for _ in range(len(nums))]
+        for el, count in counter.items():
+            buckets[count - 1].append(el)
+
+        # Pick top-k elements.
+        result = []
+        for i in reversed(range(len(nums))):
+            if buckets[i]:
+                result.extend(buckets[i])
+
+            if len(result) >= k:
+                break
+
+        return result[:k]
