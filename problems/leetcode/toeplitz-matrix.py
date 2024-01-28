d3722afb715755b3ba8b12ade930a15c2a6e83bf
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-01-28 15:59:18 -0800

    Update toeplitz-matrix.py

--- problems/leetcode/toeplitz-matrix.py
+++ problems/leetcode/toeplitz-matrix.py
@@ -32,3 +32,13 @@ class Solution:
                 col += 1
 
         return True
+
+# Cleaner Solution
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i - 1][j - 1] != matrix[i][j]:
+                    return False
+
+        return True
