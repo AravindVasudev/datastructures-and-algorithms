7713e7037b5e220a3896a4312b7cb3d0aad76a33
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-01-29 01:53:14 -0800

    Update lru-cache.py

--- problems/leetcode/lru-cache.py
+++ problems/leetcode/lru-cache.py
@@ -1,72 +1,68 @@
 # https://leetcode.com/problems/lru-cache/
+from dataclasses import dataclass
+
+@dataclass
 class Node:
-    def __init__(self, key, value, nxt=None, prev=None):
-        self.key = key
-        self.value = value
-        self.next = nxt
-        self.prev = prev
+    key: int = None
+    value: int = None
+    prev: "Node" = None
+    nxt: "Node" = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.map = dict()
-        
-        # Setup doubly linked list
-        self.linked_list_head = Node(None, None)
-        self.linked_list_tail = Node(None, None, None, self.linked_list_head)
-        
-        self.linked_list_head.next = self.linked_list_tail
-        
-        
+
+        # Init hashmap.
+        self.nodes = {}
+
+        # Init doubly linked-list.
+        self.head = Node()
+        self.tail = Node(None, None, self.head, None)
+        self.head.nxt = self.tail
+
 
     def get(self, key: int) -> int:
-        if key not in self.map:
+        if key not in self.nodes:
             return -1
+
+        # Move to the gotten node to the front of
+        # the list.
+        gotten = self.nodes[key]
+
+        # Remove from the list.
+        self.remove(gotten)
+
+        # Add to front of the list.
+        self.pushLeft(gotten)
+
+        return gotten.value
         
-        curNode = self.map[key]
-        
-        # Remove from list
-        curNode.prev.next = curNode.next
-        curNode.next.prev = curNode.prev
-        
-        # Add to front
-        curNode.next = self.linked_list_head.next
-        curNode.next.prev = curNode
-        
-        self.linked_list_head.next = curNode
-        curNode.prev = self.linked_list_head
-        
-        return curNode.value
 
     def put(self, key: int, value: int) -> None:
-        if key in self.map:
-            self.map[key].value = value
-            self.get(key)
-            return
+        if key in self.nodes:
+            self.remove(self.nodes[key])
 
-        if len(self.map) >= self.capacity:
-            nodeToEvict = self.linked_list_tail.prev
-            
-            # Remove the node from list
-            self.linked_list_tail.prev = nodeToEvict.prev
-            nodeToEvict.prev.next = self.linked_list_tail
-            
-            
-            # Remove from map
-            del self.map[nodeToEvict.key]
-        
-        #  Add to front
-        curNode = Node(key, value, self.linked_list_head.next, self.linked_list_head)
-        self.linked_list_head.next = curNode
-        curNode.next.prev = curNode
-        
-        # Add to map
-        self.map[key] = curNode
-        
-        
+        # Create a node and insert in the front.
+        newNode = Node(key, value)
+        self.pushLeft(newNode)
 
+        # Add to hash table.
+        self.nodes[key] = newNode
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        # On overflow, kick out the least recently
+        # used node.
+        if len(self.nodes) > self.capacity:
+            toRemove = self.tail.prev
+
+            self.remove(toRemove)
+            del self.nodes[toRemove.key]
+
+    def remove(self, node: Node) -> None:
+        node.prev.nxt = node.nxt
+        node.nxt.prev = node.prev
+
+    def pushLeft(self, node: Node) -> None:
+        node.prev, node.nxt = self.head, self.head.nxt
+        self.head.nxt.prev = node
+        self.head.nxt = node
