f7c58d644eb0a29ae8ab2683ce684091392626dc
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-02-03 13:55:48 -0800

    Create remove-element.py

---
+++ problems/remove-element.py
@@ -0,0 +1,10 @@
+ # https://leetcode.com/problems/remove-element/
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+
+        return i
