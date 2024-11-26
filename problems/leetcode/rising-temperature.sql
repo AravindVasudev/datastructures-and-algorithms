43096df13b0db2fc6b9ed2502a65e37ac6fc3639
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-11-26 12:37:49 -0800

    Create rising-temperature.sql

---
+++ problems/leetcode/rising-temperature.sql
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/rising-temperature/
+SELECT
+    w1.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
