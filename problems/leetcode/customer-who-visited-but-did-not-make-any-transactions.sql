92484a1d5cd019aefe7f07a00255745cbb29c18c
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-11-26 01:37:20 -0800

    Create customer-who-visited-but-did-not-make-any-transactions.sql

---
+++ problems/leetcode/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+SELECT
+    v.customer_id,
+    COUNT(v.customer_id) AS count_no_trans
+FROM
+    Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
