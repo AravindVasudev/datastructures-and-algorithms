f45247a1580ffbb6ea1f17cd9b52cd4363c180d5
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2024-12-06 23:32:54 -0800

    Update day_7.cc

--- problems/AdventOfCode2024/day_7.cc
+++ problems/AdventOfCode2024/day_7.cc
@@ -1,17 +1,42 @@
 /*
+* Instructions for Apple Silicon (M1 Pro):
+
+```
+$ brew install folly
+$ g++ -std=c++20 -I /opt/homebrew/Cellar/folly/2024.12.02.00/include -I
+/opt/homebrew/Cellar/boost/1.86.0/include -I
+/opt/homebrew/Cellar/glog/0.6.0/include -I
+/opt/homebrew/Cellar/gflags/2.2.2/include -I
+/opt/homebrew/Cellar/double-conversion/3.3.0/include -I
+/opt/homebrew/Cellar/fmt/11.0.2/include -I
+/opt/homebrew/Cellar/libevent/2.1.12_1/include -L
+/opt/homebrew/Cellar/folly/2024.12.02.00/lib -L
+/opt/homebrew/Cellar/boost/1.86.0/lib -L /opt/homebrew/Cellar/glog/0.6.0/lib -L
+/opt/homebrew/Cellar/gflags/2.2.2/lib -L
+/opt/homebrew/Cellar/double-conversion/3.3.0/lib -L
+/opt/homebrew/Cellar/fmt/11.0.2/lib -L
+/opt/homebrew/Cellar/libevent/2.1.12_1/lib -lfolly -lglog -O3 day_7.cc -o day_7
+$ ./day_7
+Part 1: 8401132154762
+Part 2: 95297119227552
+
 $ hyperfine --runs 10 ./day_7
 Benchmark 1: ./day_7
-  Time (mean ± σ):     663.7 ms ±   2.0 ms    [User: 657.6 ms, System: 5.2 ms]
-  Range (min … max):   660.8 ms … 666.4 ms    10 runs
+  Time (mean ± σ):      20.7 ms ±   0.8 ms    [User: 74.5 ms, System: 5.0 ms]
+  Range (min … max):    19.5 ms …  22.3 ms    10 runs
+```
+
 */
+
+#include <folly/executors/CPUThreadPoolExecutor.h>
+#include <folly/futures/Future.h>
 
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
-std::pair<long long, std::vector<long long>> parseEquation(
-    const std::string& equation) {
+std::pair<long, std::vector<long>> parseEquation(const std::string& equation) {
   std::istringstream ss(equation);
   std::string line;
 
@@ -21,7 +46,7 @@ std::pair<long long, std::vector<long long>> parseEquation(
   getline(ss, line, ' ');
 
   // Parse lhs.
-  std::vector<long long> lhs;
+  std::vector<long> lhs;
   while (getline(ss, line, ' ')) {
     lhs.push_back(std::stol(line));
   }
@@ -29,8 +54,8 @@ std::pair<long long, std::vector<long long>> parseEquation(
   return {rhs, lhs};
 }
 
-bool isSolvable(const std::pair<long long, std::vector<long long>>& equation,
-                size_t i = 0, long long total = 0) {
+bool isSolvable(const std::pair<long, std::vector<long>>& equation,
+                const size_t i = 0, const long total = 0) {
   if (i == equation.second.size()) {
     return total == equation.first;
   }
@@ -39,13 +64,18 @@ bool isSolvable(const std::pair<long long, std::vector<long long>>& equation,
          isSolvable(equation, i + 1, total * equation.second[i]);
 }
 
-long long concat(const long long num1, const long long num2) {
-  return std::stol(std::to_string(num1) + std::to_string(num2));
+long concat(const long num1, const long num2) {
+  // return std::stol(std::to_string(num1) + std::to_string(num2));
+  long pow = 10;
+  while (num2 >= pow) {
+    pow *= 10;
+  }
+
+  return num1 * pow + num2;
 }
 
-bool isSolvableWithConcat(
-    const std::pair<long long, std::vector<long long>>& equation,
-    const size_t i = 0, const long long total = 0) {
+bool isSolvableWithConcat(const std::pair<long, std::vector<long>>& equation,
+                          const size_t i = 0, const long total = 0) {
   if (i == equation.second.size()) {
     return total == equation.first;
   }
@@ -56,28 +86,54 @@ bool isSolvableWithConcat(
                               concat(total, equation.second[i]));
 }
 
-long long part1(const std::vector<std::pair<long long, std::vector<long long>>>&
-                    equations) {
-  long long result{};
-  for (const auto& equation : equations) {
-    if (isSolvable(equation)) {
-      result += equation.first;
+long part1(const std::vector<std::pair<long, std::vector<long>>>& equations) {
+  const int N = equations.size();
+  long total{};
+
+  const auto numCores = std::thread::hardware_concurrency();
+  folly::CPUThreadPoolExecutor executor(numCores);
+  std::vector<folly::Future<std::pair<bool, size_t>>> futures;
+  
+  for (int i = 0; i < N; i++) {
+    futures.push_back(folly::via(&executor, [&equations, i]() {
+      return std::make_pair(isSolvable(equations[i]), i);
+    }));
+  }
+
+  auto results = folly::collectAll(futures).get();  // wait.
+  for (const auto& r : results) {
+    auto res = r.value();
+    if (res.first) {
+      total += equations[res.second].first;
     }
   }
 
-  return result;
+  return total;
 }
 
-long long part2(const std::vector<std::pair<long long, std::vector<long long>>>&
-                    equations) {
-  long long result{};
-  for (const auto& equation : equations) {
-    if (isSolvableWithConcat(equation)) {
-      result += equation.first;
+long part2(const std::vector<std::pair<long, std::vector<long>>>& equations) {
+  const int N = equations.size();
+  long total{};
+
+  const auto numCores = std::thread::hardware_concurrency();
+  folly::CPUThreadPoolExecutor executor(numCores);
+  std::vector<folly::Future<std::pair<bool, size_t>>> futures;
+  
+  for (int i = 0; i < N; i++) {
+    futures.push_back(folly::via(&executor, [&equations, i]() {
+      return std::make_pair(isSolvableWithConcat(equations[i]), i);
+    }));
+  }
+
+  auto results = folly::collectAll(futures).get();  // wait.
+  for (const auto& r : results) {
+    auto res = r.value();
+    if (res.first) {
+      total += equations[res.second].first;
     }
   }
 
-  return result;
+  return total;
 }
 
 int main() {
@@ -87,7 +143,7 @@ int main() {
     return 1;
   }
 
-  std::vector<std::pair<long long, std::vector<long long>>> equations;
+  std::vector<std::pair<long, std::vector<long>>> equations;
   std::string line;
 
   while (getline(input, line)) {
