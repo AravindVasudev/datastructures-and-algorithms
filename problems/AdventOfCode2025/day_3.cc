881aa5e5832b4cbc34874dd19831e9cd83bce260
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2025-12-02 22:30:19 -0800

    [AOC 2025] Day 3 Part 2

--- problems/AdventOfCode2025/day_3.cc
+++ problems/AdventOfCode2025/day_3.cc
@@ -1,33 +1,65 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <queue>
 #include <sstream>
 #include <string>
 #include <vector>
 
-inline long ctol(const char c) {
-  return c - '0';
-}
-
-// long findMax(const std::string& bank, size_t index = 0) {
-//   if (index == bank.size()) {
-//     return -1;
-//   }
-
-
-// }
-
-long part1(const std::vector<std::string>& banks) {
-  long result{};
-  for (const auto& bank : banks) {
-    const auto size = bank.size();
-    long max = 0;
-    for (auto i = 0; i < size; i++) {
-      for (auto j = i + 1; j < size; j++) {
-        max = std::max(max, ctol(bank[i]) * 10 + ctol(bank[j]));
-      }
+struct PairComparator {
+  bool operator()(const std::pair<int, int>& a,
+                  const std::pair<int, int>& b) const {
+    if (a.first != b.first) {
+      return a.first < b.first; // Descending by value.
     }
 
-    result += max;
+    return a.second > b.second; // Ascending by index.
+  }
+};
+
+inline long ctol(const char c) { return c - '0'; }
+
+long findMax(const std::string& bank, int length) {
+  const auto size = bank.size();
+  if (size <= length) {
+    return std::stol(bank);
+  }
+
+  // We need to pick max from (size - length) and repeat until all the length
+  // digits are picked.
+  std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>,
+                      PairComparator>
+      window;
+  auto windowSize = size - length;
+  for (auto i = 0; i < windowSize; i++) {
+    window.push({ctol(bank[i]), i});
+  }
+
+  std::string max;
+  int prevIndex = -1;
+  for (auto i = windowSize; i < size; i++) {
+    // Add next digit.
+    window.push({ctol(bank[i]), i});
+
+    // clear values from before the previous picked index.
+    while (prevIndex > window.top().second) {
+      window.pop();
+    }
+
+    // Find the largest next digit.
+    auto [val, index] = window.top();
+
+    max += std::to_string(val);
+    prevIndex = index;
+    window.pop();
+  }
+
+  return std::stol(max);
+}
+
+long findMaxJoltageSum(const std::vector<std::string>& banks, int length) {
+  long result{};
+  for (const auto& bank : banks) {
+    result += findMax(bank, length);
   }
 
   return result;
@@ -46,10 +78,6 @@ int main() {
     banks.push_back(line);
   }
 
-  for (auto& bank : banks) {
-    std::cout << bank << std::endl;
-  }
-
-  std::cout << "Part 1: " << part1(banks) << std::endl;
-  std::cout << "Part 2: " << "b" << std::endl;
+  std::cout << "Part 1: " << findMaxJoltageSum(banks, 2) << std::endl;
+  std::cout << "Part 2: " << findMaxJoltageSum(banks, 12) << std::endl;
 }
